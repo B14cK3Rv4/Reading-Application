fd9a18aa7d84dcd8e470f35e405fb8c1b6de764c
B14cK3Rv4 <v4lexbl4ck@gmail.com>
2023-04-26 01:58:36 +0100

    Delete option

--- app/src/main/java/uk/readingapplication/DetailActivity.java
+++ app/src/main/java/uk/readingapplication/DetailActivity.java
@@ -2,16 +2,27 @@ package uk.readingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.github.clans.fab.FloatingActionButton;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class DetailActivity extends AppCompatActivity {
 
     TextView detailDesc, detailTitle, detailLang;
     ImageView detailImage;
+    FloatingActionButton deleteButton;
+    String key = "";
     String imageUrl = "";
 
 
@@ -25,16 +36,35 @@ public class DetailActivity extends AppCompatActivity {
         detailTitle = findViewById(R.id.detailTitle);
         detailLang = findViewById(R.id.detailLang);
         detailImage = findViewById(R.id.detailImage);
-
+        deleteButton = findViewById(R.id.deleteButton);
 
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
             detailDesc.setText(bundle.getString("Description"));
             detailTitle.setText(bundle.getString("Title"));
             detailLang.setText(bundle.getString("Language"));
+            key = bundle.getString("Key");
             imageUrl = bundle.getString("Image");
 
             Glide.with(this).load(bundle.getString("Image")).into(detailImage);
         }
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DatabaseReference reference = FirebaseDatabase.getInstance("https://readingapplication-c4df8-default-rtdb.europe-west1.firebasedatabase.app").getReference("Android Content");
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+
+                StorageReference storageReference = storage.getReferenceFromUrl(imageUrl);
+                storageReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        reference.child(key).removeValue();
+                        Toast.makeText(DetailActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                });
+            }
+        });
     }
 }
