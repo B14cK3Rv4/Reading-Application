5fd4c4ec57ed2881017704fa8cee911d0bd1a581
B14cK3Rv4 <v4lexbl4ck@gmail.com>
2023-04-26 01:33:28 +0100

    Search option

--- app/src/main/java/uk/readingapplication/MyAdapter.java
+++ app/src/main/java/uk/readingapplication/MyAdapter.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -61,6 +62,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     public int getItemCount() {
         return dataList.size();
     }
+
+    public void searchDataList(ArrayList<DataClass> searchList){
+        dataList = searchList;
+        notifyDataSetChanged();
+    }
+
+
 }
 
 class MyViewHolder extends RecyclerView.ViewHolder{
